74e846a5f4615a66df7c662666c7090cbd3c278c
shamimWP <reza@ipvision-inc.com>
2016-04-06 16:12:27 +0600

    first

---
+++ ContextMenu/MainPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using ContextMenu.Resources;
+
+namespace ContextMenu
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBackKeyPress(System.ComponentModel.CancelEventArgs e)
+        {
+            if (isContextMenuOpened)
+            {
+                isContextMenuOpened = false;
+                e.Cancel = true;
+            }
+        }
+
+        bool isContextMenuOpened;
+        private void ContextMenu_opened(object sender, RoutedEventArgs e)
+        {
+            isContextMenuOpened = true;
+        }
+
+        private void ContextMenu_closed(object sender, RoutedEventArgs e)
+        {
+            isContextMenuOpened = false;
+        }
+
+        private void ShowContextMenu_OnTap(object sender, GestureEventArgs e)
+        {
+            var moreOptions = sender as Button;
+            Microsoft.Phone.Controls.ContextMenu contextMenu = ContextMenuService.GetContextMenu(moreOptions);
+            if (contextMenu.Parent == null)
+            {
+                contextMenu.IsOpen = true;
+                isContextMenuOpened = true;
+            }
+        }
+
+        private void ContextMenuItemChat_Click(object sender, RoutedEventArgs e)
+        {
+            string header = (sender as MenuItem).Header.ToString();
+                        
+            if (header == "edit")
+            {
+                //add your action here
+            }
+
+            if (header == "delete")
+            {
+                //add your action here                
+            }
+
+            if (header == "copy")
+            {
+                //add your action here
+            }
+        }
+
+        private void OpnenContextMenu_OnHold(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            MenuItem editMenu = new MenuItem() { Header = "edit" };
+            editMenu.Click += ContextMenuItemChat_Click;
+
+            MenuItem deleteMenu = new MenuItem() { Header = "delete" };
+            deleteMenu.Click += ContextMenuItemChat_Click;
+
+            MenuItem copyMenu = new MenuItem() { Header = "delete" };
+            copyMenu.Click += ContextMenuItemChat_Click;
+
+            Microsoft.Phone.Controls.ContextMenu contextMenu = new Microsoft.Phone.Controls.ContextMenu() { VerticalAlignment = VerticalAlignment.Center, HorizontalAlignment = HorizontalAlignment.Left, IsFadeEnabled = false, IsZoomEnabled = false, IsOpen = true };
+
+            contextMenu.Items.Add(editMenu);
+            contextMenu.Items.Add(deleteMenu);
+            contextMenu.Items.Add(copyMenu);
+
+            ContextMenuService.SetContextMenu(sender as StackPanel, contextMenu);
+
+        }
+
+    }
+}
---
+++ ContextMenu/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ContextMenu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ContextMenu")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("01099e9b-468f-4fd7-b4a0-e94a55339178")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
